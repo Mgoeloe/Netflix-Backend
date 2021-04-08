671e18c164d8f015c1cdc5e8322d3668257f2e6e
Vanessa <v.braumuller@gmail.com>
2021-04-08 15:55:10 +0200

    Springboot project start

---
+++ Popcorn-Time/src/main/java/com/example/Popcorn/Time/PopcornTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Popcorn.Time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornTimeApplication.class, args);
+	}
+
+}
---
+++ Popcorn-Time/src/test/java/com/example/Popcorn/Time/PopcornTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Popcorn.Time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
