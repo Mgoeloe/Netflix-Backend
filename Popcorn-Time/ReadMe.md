e08a34a8d95d3b8387378435a582c419555f1208
Vanessa <v.braumuller@gmail.com>
2021-04-21 11:10:04 +0200

    feature2

---
+++ Popcorn-Time/ReadMe.md
@@ -0,0 +1,20 @@
+# Movie-Service (by Popcorn Time-Team)
+A Netflix clone project made by the Popcorn Time Team!
+------------------------------------------------------
+
+
+##Content-Type
+All resources are represented in JSON
+
+
+## discover page
+api/movies/genre - list of all genres
+api/movies/{id} - one movie
+
+## movie page
+api/movies/discover - list of all movies
+
+--------------------------------------------------------
+
+
+
--- Popcorn-Time/src/main/java/com/NetflixCloneProject/Popcorn/Time/Controler/ApiControler.java
+++ Popcorn-Time/src/main/java/com/NetflixCloneProject/Popcorn/Time/Controler/ApiControler.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import java.util.Optional;
 
 //@RequestMapping
 @RestController
-
+@CrossOrigin
 public class ApiControler {
 
     @Autowired
